0b9ae9bb75d2ffdd2ec39735bb81fa352b626103
Robert Fahey <rob.fahey@amido.co.uk>
2016-06-10 11:55:46 +0100

    Fixed component registration

--- source/TestStack.Seleno.BrowserStack.SpecFlowPlugin/GeneratorPlugin.cs
+++ source/TestStack.Seleno.BrowserStack.SpecFlowPlugin/GeneratorPlugin.cs
@@ -1,4 +1,5 @@
-﻿using BoDi;
+﻿using System;
+using BoDi;
 using TechTalk.SpecFlow.Generator.Configuration;
 using TechTalk.SpecFlow.Generator.Interfaces;
 using TechTalk.SpecFlow.Generator.Plugins;
@@ -12,15 +13,20 @@ using TestStack.Seleno.BrowserStack.SpecFlowPlugin;
 namespace TestStack.Seleno.BrowserStack.SpecFlowPlugin
 {
     public class GeneratorPlugin : IGeneratorPlugin
-    {       
-        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters)
-        {            
-            generatorPluginEvents.RegisterDependencies += 
-                (sender, args) => 
-                    args.ObjectContainer.RegisterInstanceAs<IUnitTestGeneratorProvider>(
+    {
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents,
+            GeneratorPluginParameters generatorPluginParameters)
+        {
+            generatorPluginEvents.CustomizeDependencies += this.GeneratorPluginEventsOnCustomizeDependencies;
+        }
+
+        private void GeneratorPluginEventsOnCustomizeDependencies(object sender, CustomizeDependenciesEventArgs customizeDependenciesEventArgs)
+        {
+            customizeDependenciesEventArgs.ObjectContainer.RegisterInstanceAs<IUnitTestGeneratorProvider>(
                         new SeleniumNUnitTestGeneratorProvider(
-                            args.ObjectContainer.Resolve<CodeDomHelper>(
-                                args.ObjectContainer.Resolve<ProjectSettings>().ProjectPlatformSettings.Language)));
+                            customizeDependenciesEventArgs.ObjectContainer.Resolve<CodeDomHelper>(
+                                customizeDependenciesEventArgs.ObjectContainer.Resolve<ProjectSettings>().ProjectPlatformSettings.Language)));
+
         }
     }
 }
